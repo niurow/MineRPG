f2de3005b4db7fec3f62211919ef849c65ae820f
nr <>
2025-02-18 15:33:51 +0800

    ad

--- web-mobile/index.b8431.js
+++ web-mobile/index.b8431.js
@@ -1,4 +1,4 @@
-System.register(["./application.32eaf.js"], function (_export, _context) {
+System.register(["./application.a5e02.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
